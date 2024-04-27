1c1d8d703aa924a0eadae781d5e8df7609b3cc89
Bedny_yorik <SlRocker@yandex.ru>
2024-04-28 01:39:50 +0300

    new data parsing

--- async_parsing.py
+++ async_parsing.py
@@ -3,32 +3,59 @@ import pandas as pd
 import httpx
 
 videos_df = pd.read_csv('./data/videos.csv')
+authors_df = pd.read_csv('./data/authors.csv')
+comments_df = pd.read_csv('./data/comments.csv')
+
 videos_urls = list(videos_df['url'])
-videos_api_urls = [f'https://rutube.ru/api/numerator/video/{url}/vote' for url in videos_urls ]
+
+videos_api_urls = [f'https://rutube.ru/api/video/{url}' for url in videos_urls]
+likes_api_urls = [f'https://rutube.ru/api/numerator/video/{url}/vote' for url in videos_urls]
+comments_api_urls = [f'https://rutube.ru/api/comments/video/{url}' for url in videos_urls]
 
 
-async def get_video_stats():
+async def get_stats(urls):
 
     async with httpx.AsyncClient(timeout=None) as client:
         results = []
-        for url in videos_api_urls:
+        for url in urls:
             try:
                 response = await client.get(url)
                 results.append(response)
             except httpx.HTTPStatusError as e:
                 if e.response.status_code == 429:
                     await asyncio.sleep(1)
-                    results.append(None)
+                    response = await client.get(url)
+                    results.append(response)
                 else:
+                    results.append(None)
                     raise
         return results
 
 
-res = asyncio.run(get_video_stats())
+async def parse():
+    info_res = await get_stats(videos_api_urls)
+    likes_res = await get_stats(likes_api_urls)
+    comments_res = await get_stats(comments_api_urls)
 
-videos_df['likes'] = [r.json().get('positive', 0) for r in res]
-videos_df['dislikes'] = [r.json().get('negative', 0) for r in res]
+    videos_df['title'] = [r.json().get('title', None) for r in info_res]
+    videos_df['description'] = [r.json().get('description', None) for r in info_res]
+    videos_df['hits'] = [r.json().get('hits', 0) for r in info_res]
+    videos_df['duration'] = [r.json().get('duration', 0) for r in info_res]
+    videos_df['likes'] = [r.json().get('positive', 0) for r in likes_res]
+    videos_df['dislikes'] = [r.json().get('negative', 0) for r in likes_res]
+    videos_df['comments_count'] = [r.json().get('comments_count', 0) for r in comments_res]
+    videos_df['author_id'] = [r.json()['author']['id'] for r in info_res]
+    videos_df['created_ts'] = [r.json().get('created_ts', 0) for r in info_res]
+    videos_df['publication_ts'] = [r.json().get('publication_ts', 0) for r in info_res]
 
-print(videos_df)
+    for comments in comments_res:
+        for comment in comments.json()['results']:
+            comments_df.loc[len(comments_df.index)] = [comment['id'], comment['video_id'], comment['user']['id'],
+                                                       comment['text'], comment['created_ts'], comment['likes_number'],
+                                                       comment['dislikes_number'], comment['replies_number']]
 
-videos_df.to_csv('./data/videos.csv', index=False)
+    videos_df.to_csv('./data/videos.csv', index=False)
+    comments_df.to_csv('./data/comments.csv', index=False)
+
+if __name__ == '__main__':
+    asyncio.run(parse())
